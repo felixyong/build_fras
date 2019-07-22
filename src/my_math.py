ad86c871f93c3c1f6334be38c564b909ff0c1dff
felix.luo <felix.luo@me.com>
2019-07-22 22:50:19 +0800

    Add doctest and unit test

---
+++ src/my_math.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*- 
+
+def square(x):
+    '''
+    Squares a number and returns the result.
+    >>> square(2)
+    4
+    >>> square(3)
+    9
+    '''
+    return x*x
+
+def product(x,y):
+    return x*y
--- src/python_test.py
+++ src/python_test.py
@@ -2,18 +2,29 @@
 #-----------------------------------------------------------------------------------
 # 
 #-----------------------------------------------------------------------------------
-name = raw_input("What is your name? ")
-print 'Hello, ' + name + '!'
-temp = 42
 
-# repr function transfer par to string
-print "The temperature is " + repr(temp)
+if __name__=='__main__':
+    import doctest, my_math
+    doctest.testmod(my_math)
+    
+import unittest, my_math
 
-# Two ways for long stance
-#print 'This is a very long string.\
-#It continues here.'
-#print '''This is a very long string.
-#It continues here.'''
+class ProductTestCase(unittest.TestCase):
+    
+    def testIntegers(self):
+        for x in xrange(-10, 10):
+            for y in xrange(-10, 10):
+                p = my_math.product(x, y)
+                self.failUnless(p == x*y, 'Integer multiplication failed')
+                
+    def testFloats(self):
+        for x in xrange(-10, 10):
+            for y in xrange(-10, 10):
+                x = x/10.0
+                y = y/10.0
+                p = my_math.product(x, y)
+                self.failUnless(p == x*y, 'Float multiplication failed')
+    
 
-# r - row string
-print r'C:\nowhere'
+if __name__ == '__main__': unittest.main()
+    
