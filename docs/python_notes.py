536725f72fe11a441e73e700f944ef87c5764ec4
felix.luo <felix.luo@me.com>
2019-08-01 00:08:18 +0800

    Update pdb and add use of yield

--- docs/python_notes.py
+++ docs/python_notes.py
@@ -25,8 +25,10 @@ print (r'C:\nowhere')
 #####################################
 """
 单步调试
-python -m pdb *.py
-
+1. python -m pdb *.py
+2. import pdb
+   pdb.set_trace()
+   
 break 或 b ：设置断点
 continue 或 c：继续执行程序
 list 或 l：查看当前行的代码段
@@ -34,6 +36,19 @@ step 或 s：进入函数
 return 或 r：执行代码直到从当前函数返回
 exit 或 q：中止并退出
 next 或 n：执行下一行
-pp：打印变量的值
+p：打印变量的值
 a：查看全部栈内变量
 """
+import pdb
+
+def foo():
+    print("starting...")
+    while True:
+        res = yield 4
+        print("res: ", res)
+
+pdb.set_trace()        
+g = foo()
+print(next(g))
+print("*"*20)
+print(next(g))
